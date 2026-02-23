d930e3990f2eed443620bfcebefa876004081824
Parmar Lakhman <119241790+lakhman108@users.noreply.github.com>
2026-02-23 14:58:14 +0530

    feat(logout): add logut

---
+++ server/logout.js
@@ -0,0 +1,17 @@
+// User Authentication System
+
+class UserAuth {
+    constructor() {
+        this.users = [];
+        this.currentUser = null;
+    }
+
+    logout(userId) {
+         const index = this.users.findIndex(u => u.id === userId);
+        this.users[index] = null;
+        return true;
+    }
+}
+
+// Export the class
+module.exports = UserAuth;
